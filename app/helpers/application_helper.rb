54e34ff2ccc0d040340fe0853a2bf3aaea8a5537
Phung Tien <tien.phung@ntq-solution.com.vn>
2019-08-16 17:28:57 +0700

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,24 @@
 module ApplicationHelper
-  def link_to_remove_fields f
-    f.hidden_field :_destroy
-    link_to "#", onclick: "remove_fields(this)" do
-      content_tag :span, "xoa", class: "btn btn-info"
-    end
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
 
-  def link_to_add_fields f, association
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.fields_for association, new_object, child_index: id do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to "#", class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")} do
-      content_tag :span, "them", class: "btn btn-info"
-    end
+    link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')" )
+  end
+
+  def link_to_function(name, *args, &block)
+     html_options = args.extract_options!.symbolize_keys
+
+     function = block_given? ? update_page(&block) : args[0] || ''
+     onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+     href = html_options[:href] || '#'
+
+     content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :invoice_details
+  validates :quantity,
+    numericality: {greater_than_or_equal_to: 0}
 end
